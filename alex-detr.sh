f4011e0500fc9fe682b63df7cbda055a718e7cf5
eak <eak@ivalua.com>
2025-04-21 09:57:48 +0000

    Add training script for object detection using DETR
    - Implemented a new shell script (alex-detr.sh) to facilitate the training of the DETR model for object detection.
    - Configured various training parameters including class threshold, number of classes, batch sizes, learning rate, and evaluation strategies.
    - Enabled features such as mixed precision training, gradient accumulation, and logging to Weights & Biases.
    - Set up the script to run in the background and log output to nohup.out for monitoring.

---
+++ alex-detr.sh
@@ -0,0 +1,8 @@
+nohup python object-detection-detr/train.py \
+    --cls_thr 0.1 --num_class 4 --validation_csv dataset/valid.json --image_folder . --training_csv dataset/train.json \
+    --output_dir logs --run_name cond-detr-50 --auto_find_batch_size --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 --num_train_epochs 200 --do_eval --do_train --fp16 --learning_rate 1e-4 \
+    --weight_decay 1e-5 --save_total_limit 2 --remove_unused_columns false --push_to_hub false --eval_strategy epoch \
+    --save_strategy epoch --report_to wandb --optim adamw_torch --lr_scheduler_type cosine --warmup_ratio 0.05 --data_seed 41 \
+    --save_safetensors --save_only_model --metric_for_best_model loss --load_best_model_at_end --overwrite_output_dir \
+    --dataloader_num_workers 4 --gradient_accumulation_steps &> nohup.out &
