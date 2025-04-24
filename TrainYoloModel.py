61210be6ba81ec4df9e7a57e07f3ad68c67dfc92
eak <eak@ivalua.com>
2025-04-24 12:08:25 +0000

    Refactor YOLO model training script and add image fixing notebook
    - Updated the YOLO model training script (TrainYoloModel.py):
      - Changed the model path to load a specific trained model.
      - Increased the number of training epochs from 100 to 200.
      - Adjusted patience from 10 to 20 for early stopping.
      - Modified max_det from 30 to 100 to allow more detections.
      - Increased the number of workers from 4 to 8 for data loading.
      - Enabled multi-scale training and resumed training from a checkpoint.
    
    - Added a new Jupyter notebook (00-1-FixImages.ipynb) for fixing image orientations:
      - Implemented functions to load images, check EXIF data, and save corrected images.
      - Utilized multithreading for processing a large number of images efficiently.

--- TrainYoloModel.py
+++ TrainYoloModel.py
@@ -1,13 +1,15 @@
 # Import libraries
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+import logging
 
 import pandas as pd
 import os
 from pathlib import Path
-
 from ultralytics import YOLO
+import torch
 
 # INPUT_DIRS
 INPUT_DATA_DIR = Path("dataset")
@@ -32,24 +34,10 @@ ss = pd.read_csv(INPUT_DATA_DIR / "SampleSubmission.csv")
 
 class_map = {cls: i for i, cls in enumerate(sorted(train["class"].unique().tolist()))}
 
-
-
 # Strip any spacing from the class item and make sure that it is a str
 train["class"] = train["class"].str.strip()
-
 # Map {'healthy': 2, 'cssvd': 1, anthracnose: 0}
 train["class_id"] = train["class"].map(class_map)
-
-
-train[["class", "class_id"]].value_counts()
-
-
-# Number of unique images path
-train["ImagePath"].nunique()
-
-
-
-import logging
 
 logging.basicConfig(
     level=logging.ERROR, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -57,35 +45,37 @@ logging.basicConfig(
 
 
 # Load a yolo pretrained model
-# Load a yolo pretrained model
-model = YOLO("yolo8n.pt")
+# model = YOLO("yolov8n.pt")
+# model = YOLO("yolo11n.pt")
+model = YOLO("runs/detect/train2/weights/best.pt")
 # model = YOLO("runs/detect/train4/weights/best.pt")
 
 # Fine tune model to our data
 model.train(
     data="data.yaml",  # Path to the dataset configuration
-    epochs=100,  # Number of epochs
+    epochs=200,  # Number of epochs
     imgsz=1024,  # Image size (height, width)
     batch=32,  # Batch size
-    patience=10,
-    # device=(
-    #     list(range(torch.cuda.device_count())) if torch.cuda.is_available() else "cpu"
-    # ),  # Use all available GPUs
-    device="cuda",  # Use the first GPU
+    patience=20,
+    device=(
+        list(range(torch.cuda.device_count())) if torch.cuda.is_available() else "cpu"
+    ),  # Use all available GPUs
+    # device="cuda",  # Use the first GPU
     multi_scale=True,
     # cos_lr=True,
-    box=9,
+    # box=9,
     dropout=0.1,
     mixup=0.1,
-    max_det=30,
+    max_det=100,
     nms=True,
-    workers=4,
+    workers=8,
     flipud=0.3,
     cls=1.0,
     augment=True,
     copy_paste=0.1,
     copy_paste_mode="mixup",
     auto_augment="augmix",
+    resume=True
 )
 
 from glob import glob
